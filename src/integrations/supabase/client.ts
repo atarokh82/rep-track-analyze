66128e79f40f2c6e55135ffa59a96f86f6d36a71
Ali Tarokh <atarokh@gmail.com>
2025-06-15 17:35:32 -0700

    hardcoded again

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,10 +2,11 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
+/*
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!SUPABASE_URL) throw new Error('Missing VITE_SUPABASE_URL environment variable');
 if (!SUPABASE_ANON_KEY) throw new Error('Missing VITE_SUPABASE_ANON_KEY environment variable');
-
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
+*/
+export const supabase = createClient<Database>('https://rfaopncdcgmkpdwaqgjg.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5ncCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmYW9wbmNkY2dta3Bkd2FxZ2pnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk1NzgxNTYsImV4cCI6MjA2NTE1NDE1Nn0.69O2xq-OaRF1f6d4Qd-NI3hOVl-o1fjAMPZDCgHWnqo'');
